ccbe532302b04275469a7a58bb260522448d9834
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-05-17 21:36:23 +0000

    readme formating

--- Grass_masks_function.py
+++ Grass_masks_function.py
@@ -9,4 +9,6 @@ import gdal
 ds = gdal.Open("/Users/kunal/Desktop/sat_test/"+str(p)+".tif")
 band1 = ds.GetRasterBand(1).ReadAsArray()    #Red
 band2 = ds.GetRasterBand(2).ReadAsArray()    # Green 
-band3 = d
+band3 = ds.GetRasterBand(3).ReadAsArray()    #blue
+band4 = ds.GetRasterBand(4).ReadAsArray()
+band5 = np.zeros((ba
