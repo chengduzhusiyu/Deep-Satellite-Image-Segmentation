7c8bc49d090a009dbd05c38ffc1b5dfd64b96520
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-05-18 13:54:50 +0000

    Update en/SUMMARY.md

--- Grass_masks_function.py
+++ Grass_masks_function.py
@@ -16,4 +16,7 @@ band5[:,:,0] = band4/np.max(band4)
 band5[:,:,1] = band2/np.max(band2) - band5[:,:,0] 
 band5[:,:,2] = band3/np.max(band3) - band5[:,:,0] 
 
-band6 = np.zeros((band1.shape[0],band
+band6 = np.zeros((band1.shape[0],band1.shape[1]))
+for i in range(band1.shape[0]):
+    for j in range(band1.shape[1]):
+        if ((band5[i][j][1] <= 0 and band5[i][j][1] >
