419f33f5b9c6d2ba952ee9dfcda0924ef0151731
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-05-23 18:26:44 +0000

    Add react

--- Water_masks_function.py
+++ Water_masks_function.py
@@ -11,4 +11,8 @@ import gdal
 ds = gdal.Open("sat_test/"+str("name")+".tif")
 band1 = ds.GetRasterBand(1).ReadAsArray()    #Red
 band2 = ds.GetRasterBand(2).ReadAsArray()    #Green 
-band3 = ds.GetRasterBand(3).ReadAsArray()   
+band3 = ds.GetRasterBand(3).ReadAsArray()    #blue
+band4 = ds.GetRasterBand(4).ReadAsArray()
+band5 = np.zeros((nparray1.shape[0],nparray1.shape[1]))
+
+band
