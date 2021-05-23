721e0ea0b7ff47ad138f4ea2b458eef4936e3c3c
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-05-23 12:28:18 +0000

    Updated TODOs

--- Water_masks_function.py
+++ Water_masks_function.py
@@ -9,4 +9,6 @@ import gdal
 
 
 ds = gdal.Open("sat_test/"+str("name")+".tif")
-band1 = ds.GetRasterBand(1).ReadAsArray()    #Re
+band1 = ds.GetRasterBand(1).ReadAsArray()    #Red
+band2 = ds.GetRasterBand(2).ReadAsArray()    #Green 
+band3 = ds.GetRasterBand(3).ReadAsArray()   
