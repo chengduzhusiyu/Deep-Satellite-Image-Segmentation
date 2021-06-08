f893ff5525793ba126157db5be887b0b7c1002f7
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-06-08 22:55:56 +0000

    old

--- gen_patches.py
+++ gen_patches.py
@@ -3,4 +3,5 @@ import numpy as np
 
 def get_rand_patch(img, mask, sz=160):
     """
-    :para
+    :param img: ndarray with shape (x_sz, y_sz, num_channels)
+    :param mask: binary ndarray with shape (x_sz, y_sz, num_clas
