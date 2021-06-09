67bb8ee1e262ac44eaea3bcaae45b80d11a813fa
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-06-09 17:45:47 +0000

    add travis status image

--- gen_patches.py
+++ gen_patches.py
@@ -4,4 +4,9 @@ import numpy as np
 def get_rand_patch(img, mask, sz=160):
     """
     :param img: ndarray with shape (x_sz, y_sz, num_channels)
-    :param mask: binary ndarray with shape (x_sz, y_sz, num_clas
+    :param mask: binary ndarray with shape (x_sz, y_sz, num_classes)
+    :param sz: size of random patch
+    :return: patch with shape (sz, sz, num_channels)
+    """
+    try:
+       
