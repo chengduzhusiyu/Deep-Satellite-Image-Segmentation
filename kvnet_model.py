ee477f401371378d8a96ce02a246e6c1cad61fc3
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-07-07 12:30:58 +0000

    Finish implementation

--- kvnet_model.py
+++ kvnet_model.py
@@ -1,1 +1,4 @@
-# u-net model with up-convolution or up-sampling and weigh
+# u-net model with up-convolution or up-sampling and weighted binary-crossentropy as loss func
+
+from keras.models import Model
+from keras.layers import
