9be8fe3f51407d826d7a612dfc704d9182f681ae
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-07-08 18:15:54 +0000

    Remove duplicated files

--- kvnet_model.py
+++ kvnet_model.py
@@ -8,4 +8,6 @@ from keras import backend as K
 
 
 def unet_model(n_classes=8, im_sz=160, n_channels=4, n_filters_start=32, growth_factor=2, upconv=True,
-           
+               class_weights=[0.2, 0.3, 0.1, 0.1, 0.3]):
+    droprate=0.25
+    n_filters = n_filters_st
