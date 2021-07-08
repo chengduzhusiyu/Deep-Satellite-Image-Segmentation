b4edba7857556f2844bb1a8e8d3e03a57cade707
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-07-08 15:44:11 +0000

    Update from Gnulib

--- kvnet_model.py
+++ kvnet_model.py
@@ -7,4 +7,5 @@ from keras.utils import plot_model
 from keras import backend as K
 
 
-def 
+def unet_model(n_classes=8, im_sz=160, n_channels=4, n_filters_start=32, growth_factor=2, upconv=True,
+           
