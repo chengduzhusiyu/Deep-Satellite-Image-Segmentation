5cf563e0f0d955fdadd7c112a91ec2122a63c8ab
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-07-26 20:40:51 +0000

    style: docs

--- kvnet_model.py
+++ kvnet_model.py
@@ -43,4 +43,7 @@ def unet_model(n_classes=8, im_sz=160, n_channels=4, n_filters_start=32, growth_
     pool4_1 = BatchNormalization()(pool4_1)
     conv4_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool4_1)
     conv4_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv4_1)
-    pool4_2
+    pool4_2 = MaxPooling2D(pool_size=(2, 2))(conv4_1)
+    pool4_2 = Dropout(droprate)(pool4_2)
+
+    n_filters *= 
