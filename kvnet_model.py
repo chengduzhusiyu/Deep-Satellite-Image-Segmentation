d6c975184308e076df267816f3834be945d5aa93
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-07-28 06:07:59 +0000

    Update Finnish translations.

--- kvnet_model.py
+++ kvnet_model.py
@@ -52,4 +52,6 @@ def unet_model(n_classes=8, im_sz=160, n_channels=4, n_filters_start=32, growth_
 
     n_filters //= growth_factor
     if upconv:
-        up6_1 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(c
+        up6_1 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv5), conv4_1])
+    else:
+        up6_1 = concatenate([UpSampling2D(siz
