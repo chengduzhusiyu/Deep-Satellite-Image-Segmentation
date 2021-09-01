a92d49c36e8fe14e8ad293fee31c6bd2321ce966
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-09-01 08:59:00 +0000

    libceph: move msgr clear_standby under con mutex protection

--- kvnet_model.py
+++ kvnet_model.py
@@ -106,4 +106,5 @@ def unet_model(n_classes=8, im_sz=160, n_channels=4, n_filters_start=32, growth_
         class_loglosses = K.mean(K.binary_crossentropy(y_true, y_pred), axis=[0, 1, 2])
         return K.sum(class_loglosses * K.constant(class_weights))
 
-    model.compile(optimizer=Adam(), lo
+    model.compile(optimizer=Adam(), loss=weighted_binary_crossentropy)
+    return model
---
+++ predict.py
@@ -0,0 +1,35 @@
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import tifffile as tiff
+
+
+from train_unet import get_model, normalize, PATCH_SZ, N_CLASSES 
+
+
+def predict(x, model, patch_sz=160, n_classes=8):
+    img_height = x.shape[0]
+    img_width = x.shape[1]
+    n_channels = x.shape[2]
+    # make extended img so that it contains integer number of patches
+    npatches_vertical = math.ceil(img_height / patch_sz)
+    npatches_horizontal = math.ceil(img_width / patch_sz)
+    extended_height = patch_sz * npatches_vertical
+    extended_width = patch_sz * npatches_horizontal
+    ext_x = np.zeros(shape=(extended_height, extended_width, n_channels), dtype=np.float32)
+    # fill extended image with mirrors:
+    ext_x[:img_height, :img_width, :] = x
+    for i in range(img_height, extended_height):
+        ext_x[i, :, :] = ext_x[2 * img_height - i - 1, :, :]
+    for j in range(img_width, extended_width):
+        ext_x[:, j, :] = ext_x[:, 2 * img_width - j - 1, :]
+
+    # now we assemble all patches in one array
+    patches_list = []
+    for i in range(0, npatches_vertical):
+        for j in range(0, npatches_horizontal):
+            x0, x1 = i * patch_sz, (i + 1) * patch_sz
+            y0, y1 = j * patch_sz, (j + 1) * patch_sz
+            patches_list.append(ext_x[x0:x1, y0:y1, :])
+    # model.predict() needs numpy array rather than a list
