a164a52a8e20b62267d8d56fc7d859e2e0047a44
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-09-06 13:09:35 +0000

    Update headers

--- predictkvnet.py
+++ predictkvnet.py
@@ -4,4 +4,9 @@ import matplotlib.pyplot as plt
 import tifffile as tiff
 import cv2
 
-from train_KVnet import weights_path, get_model, normalize, PATC
+from train_KVnet import weights_path, get_model, normalize, PATCH_SZ, N_CLASSES
+
+
+def predict(x, model, patch_sz=160, n_classes=8):
+    img_height = x.shape[0]
+    img_w
