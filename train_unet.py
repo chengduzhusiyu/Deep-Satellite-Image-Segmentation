01fdb452345377a547f74edf3818f35784a7e1e1
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-10-26 10:55:09 +0000

    f2fs: relocate is_merged_page

--- train_unet.py
+++ train_unet.py
@@ -6,4 +6,11 @@ import numpy as np
 import tifffile as tiff
 from keras.callbacks import CSVLogger
 from keras.callbacks import TensorBoard
-from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlate
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+
+def normalize(img):
+    min = img.min()
+    max = img.max()
+    x = 2.0 * (img - min) / (max - min) - 1.0
+    re
