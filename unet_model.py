ed8d2d611317543d9fb1d726bfe850ff567b63f3
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-11-27 19:58:52 +0000

    react

--- unet_model.py
+++ unet_model.py
@@ -2,4 +2,9 @@
 
 from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout
-from keras.optimizers import 
+from keras.optimizers import Adam
+from keras.utils import plot_model
+from keras import backend as K
+
+
+def unet_model(n_classes=8, im_sz=16
