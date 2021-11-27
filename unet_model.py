5dc7058df6071b030f4345f3ff8b8e712ed02910
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-11-27 11:28:52 +0000

    Mi primer commit

--- unet_model.py
+++ unet_model.py
@@ -1,4 +1,5 @@
 # u-net model with up-convolution or up-sampling and weighted binary-crossentropy as loss func
 
 from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, U
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout
+from keras.optimizers import 
