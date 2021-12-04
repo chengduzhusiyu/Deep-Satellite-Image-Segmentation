f7c83e5e07190ea8e19f540c121a3a35d9899e7a
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-12-04 18:49:54 +0000

    New tweet via web frontend uploaded

--- unet_model.py
+++ unet_model.py
@@ -20,4 +20,5 @@ def unet_model(n_classes=8, im_sz=160, n_channels=4, n_filters_start=32, growth_
 
     n_filters *= growth_factor
     pool1 = BatchNormalization()(pool1)
-    conv2 = C
+    conv2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(n_filters,
