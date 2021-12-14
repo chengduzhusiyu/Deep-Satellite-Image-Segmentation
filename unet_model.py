e81443e3369dd23121527dee558289016ffb1701
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-12-14 13:49:02 +0000

    Fixed some comments.

--- unet_model.py
+++ unet_model.py
@@ -52,4 +52,4 @@ def unet_model(n_classes=8, im_sz=160, n_channels=4, n_filters_start=32, growth_
 
     n_filters //= growth_factor
     if upconv:
-     
+        up6_1 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(con
