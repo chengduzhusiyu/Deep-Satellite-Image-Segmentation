52f58a3c43bfd0dfdb9c1fa3d32c79ca31acabfe
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-01-03 09:15:54 +0000

    Modeling

--- unet_model.py
+++ unet_model.py
@@ -103,4 +103,7 @@ def unet_model(n_classes=8, im_sz=160, n_channels=4, n_filters_start=32, growth_
     model = Model(inputs=inputs, outputs=conv10)
 
     def weighted_binary_crossentropy(y_true, y_pred):
-        class_loglosses = K.mean(K.binary_crossentropy(y_true, y_pred), axis=[0, 1, 2]
+        class_loglosses = K.mean(K.binary_crossentropy(y_true, y_pred), axis=[0, 1, 2])
+        return K.sum(class_loglosses * K.constant(class_weights))
+
+    model.compile(optimizer=Adam(), loss=wei
