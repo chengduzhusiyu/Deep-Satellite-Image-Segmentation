406010e6d3953ae5f2c3540ed7fbcbf5fe1dbced
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-01-03 09:56:58 +0000

    SAK-15773

---
+++ unet_model_deeper.py
@@ -0,0 +1,55 @@
+
+# u-net model with up-convolution or up-sampling and weighted binary-crossentropy as loss func
+
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout
+from keras.optimizers import Adam
+from keras.utils import plot_model
+from keras import backend as K
+
+
+def unet_model(n_classes=5, im_sz=320, n_channels=8, n_filters_start=32, growth_factor=2, upconv=True,
+               class_weights=[0.2, 0.3, 0.1, 0.1, 0.3]):
+    droprate=0.25
+    n_filters = n_filters_start
+    inputs = Input((im_sz, im_sz, n_channels))
+    #inputs = BatchNormalization()(inputs)
+    conv1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    pool1 = Dropout(droprate)(pool1)
+
+    n_filters *= growth_factor
+    pool1 = BatchNormalization()(pool1)
+    conv2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    pool2 = Dropout(droprate)(pool2)
+
+    n_filters *= growth_factor
+    pool2 = BatchNormalization()(pool2)
+    conv3 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    pool3 = Dropout(droprate)(pool3)
+
+    n_filters *= growth_factor
+    pool3 = BatchNormalization()(pool3)
+    conv4_0 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool3)
+    conv4_0 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv4_0)
+    pool4_0 = MaxPooling2D(pool_size=(2, 2))(conv4_0)
+    pool4_0 = Dropout(droprate)(pool4_0)
+
+    n_filters *= growth_factor
+    pool4_0 = BatchNormalization()(pool4_0)
+    conv4_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool4_0)
+    conv4_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv4_1)
+    pool4_1 = MaxPooling2D(pool_size=(2, 2))(conv4_1)
+    pool4_1 = Dropout(droprate)(pool4_1)
+
+    n_filters *= growth_factor
+    pool4_1 = BatchNormalization()(pool4_1)
+    conv4_2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool4_1)
+    conv4_2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv4_2)
+    pool4_2 = MaxPooling2D(pool_size=(2, 2))(conv4_2)
+    pool4_2 = Dropout(droprate)(pool4_2)
